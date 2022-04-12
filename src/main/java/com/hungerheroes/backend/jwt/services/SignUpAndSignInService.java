83f3f952e200c0ad1a777ad7671dc61e3f2826a0
Abdullah AL Habib <aalhabib001@gmail.com>
2022-04-12 20:58:39 +0600

    added

--- src/main/java/com/hungerheroes/backend/jwt/services/SignUpAndSignInService.java
+++ src/main/java/com/hungerheroes/backend/jwt/services/SignUpAndSignInService.java
@@ -18,16 +18,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.xml.bind.ValidationException;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @AllArgsConstructor
 @Service
